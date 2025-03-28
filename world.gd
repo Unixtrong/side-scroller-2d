c14237e43950da7bb0a3dff27883494d53936c47
Unixtrong <cloudy629@gmail.com>
2025-03-29 03:59:57 +0800

    ✨ 完成地图集设置
    Signed-off-by: Unixtrong <cloudy629@gmail.com>

--- world.gd
+++ world.gd
@@ -6,12 +6,16 @@ extends Node2D
 
 
 func _ready() -> void:
+	# 获取 TileMap2D 子场景中的 Platform 节点
 	var platform = get_node("TileMap2D/Platform") as TileMapLayer
+	# 获取已使用的矩形尺寸，单位是单元格个数
 	var used = platform.get_used_rect()
+	# 每个单元格的尺寸，单位像素
 	var tile_size = platform.tile_set.tile_size
-	
+	# 限制相机四边大小，单位像素
 	camera_2d.limit_left = used.position.x * tile_size.x
 	camera_2d.limit_top = used.position.y * tile_size.y
 	camera_2d.limit_right = used.end.x * tile_size.x
 	camera_2d.limit_bottom = used.end.y * tile_size.y
+	# 禁止游戏开始时镜头应用 limit 时的动画
 	camera_2d.reset_smoothing()
