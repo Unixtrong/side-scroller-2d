1a3bb931a96b5427aaf2a13940fb6d45df8b6ab6
Unixtrong <cloudy629@gmail.com>
2025-03-29 12:24:18 +0800

    ✨  添加加速度实现
    Signed-off-by: Unixtrong <cloudy629@gmail.com>

--- player.gd
+++ player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 const RUN_SPEED = 200.0
+const ACCELERATION = RUN_SPEED / 0.2
 const JUMP_VELOCITY = -360.0
 
 var gravity = ProjectSettings.get("physics/2d/default_gravity") as float
@@ -11,14 +12,14 @@ var gravity = ProjectSettings.get("physics/2d/default_gravity") as float
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_axis("move_left", "move_right")
-	velocity.x = direction * RUN_SPEED
+	velocity.x = move_toward(velocity.x, direction * RUN_SPEED, ACCELERATION * delta)
 	velocity.y += gravity * delta
 
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = JUMP_VELOCITY
 
 	if is_on_floor():
-		if is_zero_approx(direction):
+		if is_zero_approx(direction) and is_zero_approx(velocity.x):
 			animation_player.play("idle")
 		else:
 			animation_player.play("run")
