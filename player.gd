ce6f4683a25ed1f11522a4e87ad4c2ae7c2cacc9
Unixtrong <cloudy629@gmail.com>
2025-03-29 12:46:54 +0800

    ✨ 实现按键时长影响跳跃高速，空中转向优化
    Signed-off-by: Unixtrong <cloudy629@gmail.com>

--- player.gd
+++ player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
 const RUN_SPEED = 200.0
-const ACCELERATION = RUN_SPEED / 0.2
+const FLOOR_ACCELERATION = RUN_SPEED / 0.2
+const AIR_ACCELERATION = RUN_SPEED / 0.02
 const JUMP_VELOCITY = -360.0
 
 var gravity = ProjectSettings.get("physics/2d/default_gravity") as float
@@ -13,13 +14,16 @@ var gravity = ProjectSettings.get("physics/2d/default_gravity") as float
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if Input.is_action_pressed("jump"):
+	if event.is_action_pressed("jump"):
 		jump_request_timer.start()
+	if event.is_action_released("jump") and velocity.y < JUMP_VELOCITY / 2:
+		velocity.y = JUMP_VELOCITY / 2
 
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_axis("move_left", "move_right")
-	velocity.x = move_toward(velocity.x, direction * RUN_SPEED, ACCELERATION * delta)
+	var acceleration = FLOOR_ACCELERATION if is_on_floor() else AIR_ACCELERATION
+	velocity.x = move_toward(velocity.x, direction * RUN_SPEED, acceleration * delta)
 	velocity.y += gravity * delta
 
 	var can_jump = is_on_floor() or coyote_timer.time_left > 0
